fb77f5bfe007e860b4709272f9b8e9932f240e8f
Bai, Bin <bin.bai@hpe.com>
2016-06-07 15:40:46 +0800

    Update the logic about section.

--- service/js/section.js
+++ service/js/section.js
@@ -1,77 +1,154 @@
 var db = require('./db');
 var Promise = require('promise');
 
+// APIs about section table.
 exports.createSection = createSection;
-exports.getIdByName = getIdByName;
+exports.getSectionIdByName = getSectionIdByName;
+exports.updateSection = updateSection;
+exports.deleteSection = deleteSection;
+exports.getRootSectionId = getRootSectionId;
+
+// APIs about section section table.
 exports.createSectionSection = createSectionSection;
+exports.deleteSectionSection = deleteSectionSection;
+exports.getAllSectionSection = getAllSectionSection;
 
-function createSection(section, callback) {
-  if(section.name) {
-    var sql = "insert into section(name) values('" + section.name + "');";
-    console.info({sql});
-    db.textQuery(sql, callback);
-  } else {
-    console.error("Can't fine name in section object.", {section});
-  }    
+
+/**
+ * Create the section.
+ * 
+ */
+function createSection(section) {
+  // TODO:
+  // Check the integration of section object.
+  var sql = "insert into section(name) values('" + section.name + "');";
+  return _getPromiseOfSql(sql, null);
 }
 
-function getIdByName(name, callback) {
-  if(name) {
-    var sql = "select id from section where name='" + name + "';";
-    console.info({sql});
-    db.textQuery(sql, callback);
-  } else {
-    console.error("Can't get ID without a name.", {name});
+/**
+ * Get the section ID by name.
+ * 
+ */
+function getSectionIdByName(name) {
+  if (!name) {
+    return new Promise((resolve, reject) => {
+      reject("Can't get ID without a name.");
+    });
   }
+
+  var sql = "select id from section where name='" + name + "';";
+  return _getPromiseOfSql(sql, null);
 }
 
-function createSectionSection(parentId, childId, callback) {
-  if(! childId) {
-    console.error("Can't create section relation ship with child = null.");
-    return;
+/**
+ * Update section by ID.
+ * 
+ */
+function updateSection(id, section) {
+  // TODO:
+  // Check the integration of section object.  
+  if (!id) {
+    return new Promise((resolve, reject) => {
+      reject("Can't update section without ID.");
+    });
   }
+  
+  var sql = "update section set value name='" + section.name + "' where id='" + id + "';";
+  return _getPromiseOfSql(sql, null);
+}
+
+/**
+ * Delete section by ID.
+ * 
+ */
+function deleteSection(id) {
+  if (!id) {
+    return new Promise((resolve, reject) => {
+      reject("Can't remove section without ID.");
+    });
+  }
+
+  var sql = "delete from section where id='" + id + "';";
+  return _getPromiseOfSql(sql, null);
+}
+
+/**
+ * Create the section section relationship.
+ * 
+ */
+function createSectionSection(parentId, childId) {
+  if (!childId) {
+    return new Promise((resolve, reject) => {
+      reject("Can't create section section with child = null.");
+    });
+  }
+  
   var sql = "insert into section_section(parent, child) values(" + parentId + "," + childId + ");";
-  console.info({sql});
-  db.textQuery(sql, callback);
+  return _getPromiseOfSql(sql, null);
 }
 
+/**
+ * Delete the section section relationship.
+ * If the child has children, you can't delete the relationship.
+ * 
+ */
+function deleteSectionSection(parentId, childId) {
+  if (! childId) {
+    return new Promise((resolve, reject) => {
+      reject("Can't remove section section with child = null.");
+    });
+  }
+  
+  var getChildren = getChildrenIdBySectionId(childId);
+  var retPromise = getChildren.then((children) => {
+    if(children.lenght !== 0) {
+      return new Promise((resolve, reject) => {
+        reject("Can't remove section section if the child section have children.");
+      });
+    } else {
+      var sql = "delete into section_section where parent='" + parentId + "',child='" + childId + "';";
+      return _getPromiseOfSql(sql, null);
+    }
+  });
+  
+  return retPromise;
+}
 
-function getRootId() {
-  var promise = new Promise((resolve, reject) => {
-    var sql = "select id from section where name is null";
-    db.textQuery(sql, (err, result) => {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(result.rows[0].id);
-      }
+/**
+ * Get the root section.
+ * The name of the root section is null.
+ * 
+ */
+function getRootSectionId() {  
+  var sql = "select id from section where name is null";
+  return _getPromiseOfSql(sql, (result) => {
+    return result.rows[0].id;
+  });
+}
+
+/**
+ * Give the ID of a section, return it's children.
+ * 
+ */
+function getChildrenIdBySectionId(id) {  
+  var sql = "select id from section_section where parent='" + id + "';";
+  return _getPromiseOfSql(sql, (result) => {
+    return result.rows.map((eachRow) => {
+      return eachRow.id;
     });
   });
-  return promise;
 }
 
-function getChildrenIdByParentId(id) {
-  var promise = new Promise((resolve, reject) => {
-    var sql = "select id from section_section where parent='" + id + "';";
-    db.textQuery(sql, (err, result) => {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(result.rows.map((row) => {
-          return row.id;
-        }));
-      }
-    });
-  });
-  return promise;
-}
-
+/**
+ * Get all the section section relationship.
+ * 
+ */
 function getAllSectionSection() {
   var promise = new Promise((resolve, reject) => {
     var sql = "select * from section_section;";
     db.textQuery(sql, (err, result) => {
-      if(err) {
-        reject(err);        
+      if (err) {
+        reject(err);
       } else {
         var ret = [];
         result.rows.map((eachRow) => {
@@ -85,15 +162,37 @@ function getAllSectionSection() {
     });
   });
   return promise;
+  
+  var sql = "select * from section_section;";
+  return _getPromiseOfSql(sql, (result) => {
+    result.rows.map((eachRow) => {
+      return {
+        "parent": eachRow.parent,
+        "child": eachRow.child
+      };
+    });
+  });
 }
 
-function getSectionTree() {
-  var rootId = getRootId();
-  var tree = rootId.then((id) => {
-    
+/**
+ * Execute the SQL command and return the result.
+ * @ transformRawResult if provide, it will be used to transform the raw
+ * SQL result.
+ */
+function _getPromiseOfSql(sql, transformRawResult) {
+  if (!sql) {
+    console.error("Unable to get Promise of 'SQL = null.'");
+    return null;
+  }
+  var promise = new Promise((resolve, reject) => {
+    db.textQuery(sql, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(transformRawResult ? transformRawResult(result) : result);
+      }
+    });
   });
-
-  
-  return rootId; 
+  return promise;
 }
 
